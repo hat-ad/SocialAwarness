813a3d0afb63a7c80f765f6cd51cf7b9f12ae34f
hat-ad <dasashis672@gmail.com>
2021-02-15 23:59:57 +0530

    changed api url

--- src/API/service/api.js
+++ src/API/service/api.js
@@ -1,7 +1,7 @@
 import API from "./service";
 
-// export const baseUrl = "https://social-awarness-backend.herokuapp.com/api/";
-export const baseUrl = "http://localhost:8000/api/";
+export const baseUrl = "https://social-awarness-backend.herokuapp.com/api/";
+// export const baseUrl = "http://localhost:8000/api/";
 
 export const oAuth =
   "https://social-awarness-backend.herokuapp.com/api/users/get-token/";
